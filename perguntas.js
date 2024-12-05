a54a38ace80695fee7857bbf22dbd00cb5279d9e
baratamalasiatica <168439293+baratamalasiatica@users.noreply.github.com>
2024-12-04 22:27:12 -0300

    comi ti

---
+++ perguntas.js
@@ -0,0 +1,89 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+
+criaCartao(
+    `futebol`,
+    `melhor time brasileiro`,
+    `Mengão`
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Tnoevry Grvkrven, niryvab*/
